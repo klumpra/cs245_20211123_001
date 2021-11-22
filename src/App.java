7155f134d408bc94fb6342403c0a85a859b1f683
Ray Klump Lewis University <klumpra@lewisu.edu>
2021-11-22 10:35:42 -0600

    Added a comment to the main app.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
 	public static void main(String[] args) {
+		// this will create the frame and show it.
 		DrawChoiceFrame dcf = new DrawChoiceFrame();
 		dcf.setVisible(true);
 	}
